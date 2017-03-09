035b37febda485c150b18badb41f173ef918dfcd
Lucky-cyn <1223549862@qq.com>
2017-03-09 09:03:13 +0800

    [2017_3_9_8_57]

--- C++ workspace/ConsoleApplication10/ConsoleApplication10/标头.h
+++
@@ -1,6 +0,0 @@
-#ifndef MYCLASS
-#define MYCLASS
-int Public(int a, int b);
-
-
-#endif
--- C++ workspace/ConsoleApplication11/ConsoleApplication11/ConsoleApplication11.cpp
+++
@@ -1,8 +0,0 @@
-// ConsoleApplication11.cpp : 定义控制台应用程序的入口点。
-//
-
-#include "stdafx.h"
-#include <iostream>
-using namespace std;
-
-
